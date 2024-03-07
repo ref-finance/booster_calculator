5fa33fdc7f7425bea210edc36614d8b594733aef
MagicGordon <19465870+MagicGordon@users.noreply.github.com>
2024-03-07 20:05:51 +0800

    add decimals in funcs

--- src/burrow.rs
+++ src/burrow.rs
@@ -22,22 +22,6 @@ pub fn compute_x_booster_amount(
         )
 }
 
-
-// pub fn compute_x_booster_multiplier_at_maximum_staking_duration(
-//     maximum_staking_duration_sec: u32, 
-//     minimum_staking_duration_sec: u32,
-//     duration_ns: u64,
-//     target: u128
-// ) -> u128 {
-//     u128_ratio(
-//         target,
-//         u128::from(to_nano(
-//             maximum_staking_duration_sec - minimum_staking_duration_sec,
-//         )) * u128::from(MIN_BOOSTER_MULTIPLIER),
-//         u128::from(duration_ns - to_nano(minimum_staking_duration_sec))
-//     ) + u128::from(MIN_BOOSTER_MULTIPLIER)
-// }
-
 pub fn compute_x_booster_multiplier_at_maximum_staking_duration(
     maximum_staking_duration_sec: u32, 
     minimum_staking_duration_sec: u32,
@@ -45,6 +29,8 @@ pub fn compute_x_booster_multiplier_at_maximum_staking_duration(
     capital: u128,
     target: u128
 ) -> u128 {
+    let capital = capital * 10u128.pow(18);
+    let target = target * 10u128.pow(18);
     u128_ratio(
         target - capital,
         u128::from(to_nano(
@@ -55,6 +41,7 @@ pub fn compute_x_booster_multiplier_at_maximum_staking_duration(
 }
 
 pub fn calc_extra_shares(booster_log_base: u128, booster_balance: u128, shares: u128) -> u128 {
+    let booster_balance = booster_balance * 10u128.pow(18);
     let booster_base = 10u128.pow(18);
     let log_base =
             (booster_log_base as f64) / (booster_base as f64);
@@ -64,6 +51,7 @@ pub fn calc_extra_shares(booster_log_base: u128, booster_balance: u128, shares: 
 }
 
 pub fn calc_log_base(booster_balance: u128, shares: u128, target: u128) -> u128 {
+    let booster_balance = booster_balance * 10u128.pow(18);
     let booster_base = 10u128.pow(18);
     let extra_shares = target - shares;
     (((booster_balance / booster_base) as f64).powf(1.0f64 / (extra_shares as f64 / shares as f64)) * 10f64.powi(18)) as u128
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ pub use utils::*;
 
 fn main() {
     // burrow
-    println!("{}", compute_x_booster_multiplier_at_maximum_staking_duration(31104000, 2592000, to_nano(2592000 * 2), 100, 150));
-    println!("{}", compute_x_booster_amount(31104000, 2592000, 65000, 100, to_nano(2592000 * 2)));
+    {
+        println!("{}", compute_x_booster_multiplier_at_maximum_staking_duration(31104000, 2592000, to_nano(2592000 * 2), 100, 150));
+        println!("{}", compute_x_booster_amount(31104000, 2592000, 65000, 100, to_nano(2592000 * 2)));
 
-    println!("{}", calc_log_base(10u128.pow(20), 1000, 1200));
-    println!("{}", calc_extra_shares(9999999999999999583119736832, 10u128.pow(20), 1000));
+        println!("{}", calc_log_base(300_0000, 1000, 1300));
+        println!("{}", calc_extra_shares(340282366920938463463374607431768211455, 300_0000, 1000));
+    }
 }
